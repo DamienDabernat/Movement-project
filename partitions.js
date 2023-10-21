0f4b47860a7e47fa8bd8bd5ab51cd70404085384
Damien Dabernat <damien.dabernat@gmail.com>
2023-10-21 22:40:18 +0200

    Auto play and some css

--- partitions.js
+++ partitions.js
@@ -34,8 +34,15 @@ function getSongDetails(songName) {
     const happyBirthday = [
         "Do", "Do", "Ré", "Do", "Fa", "Mi",
         "Do", "Do", "Ré", "Do", "Sol", "Fa",
-        "Do", "Do", "Do'", "Ré", "Fa", "Mi", "Ré",
+        "Do", "Do", "Do'", "La", "Fa", "Mi", "Ré",
         "La#", "La#", "La", "Fa", "Sol", "Fa"
+    ];
+
+    const happyBirthdayRhythm = [
+        0.75, 0.25, 1, 1, 1, 2,
+        0.75, 0.25, 1, 1, 1, 2,
+        0.75, 0.25, 1, 1, 1, 1, 1,
+        0.75, 0.25, 1, 1, 1, 2
     ];
 
     const sevenNationArmy = [
@@ -44,6 +51,16 @@ function getSongDetails(songName) {
         "Mi", "Sol", "Sol", "Fa#", "Mi", "Ré", "Do",
         "Do", "Do", "Ré", "Mi"
     ];
+
+    const laCucaracha = [
+        "Do", "Ré", "Mi", "Fa", "Sol",
+        "Sol", "Fa", "Mi", "Ré", "Do",
+        "Mi", "Mi", "Fa", "Sol",
+        "Sol", "Fa", "Mi", "Ré",
+        "Do", "Ré", "Mi", "Fa", "Sol",
+        "Sol", "Fa", "Mi", "Ré", "Do"
+    ];
+
 
 
     if (songName === "Brother John") {
@@ -62,44 +79,13 @@ function getSongDetails(songName) {
         notes = fullMajorScale;
         labels = fullNoteNames;
         partition = sevenNationArmy;
+    } else if (songName === "La Cucaracha") {
+        notes = majorScale;
+        labels = noteNames;
+        partition = laCucaracha;
     } else {
         return null;
     }
 
-    return { notes : notes, labels : labels, partition : partition };
+    return { notes : notes, labels : labels, partition : partition, rhythm: happyBirthdayRhythm };
 }
-
-// Test the function
-const songDetails = getSongDetails("Brother John");
-console.log(songDetails);
-
-
-
-//const majorScale = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25];
-//const noteNames =  ["Do",   "Ré",   "Mi",   "Fa",  "Sol",   "La",   "Si",   "Do'"];
-const brotherJohn = [
-    "Do", "Ré", "Mi", "Do",
-    "Do", "Ré", "Mi", "Do",
-    "Mi", "Fa", "Sol",
-    "Mi", "Fa", "Sol",
-    "Sol", "La", "Sol", "Fa",
-    "Mi", "Do", "Mi", "Do",
-    "Do", "Sol", "La", "Si",
-    "Do", "Sol", "La", "Si",
-    "Si", "La", "Sol", "Fa",
-    "Mi", "Do", "Mi", "Do",
-    "Do", "Sol", "Do",
-    "Do", "Sol", "Do"
-];
-
-//const majorScale = [261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25];
-//const noteNames = ["Do", "Do#", "Ré", "Ré#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si", "Do'"];
-const furElise = [
-    "Mi", "Ré#", "Mi", "Ré#", "Mi", "Si", "Do", "La",
-    "Sol#", "Fa#", "Sol#", "Fa#", "Sol#", "Mi", "Do", "Si", "La",
-    "Mi", "La", "Si",
-    "Mi", "Sol#", "Si", "Do", "Ré",
-    "Mi", "La", "Si",
-    "Mi", "Sol#", "Si", "Do", "Ré",
-    "Ré#", "Do", "Si", "La", "Si", "Do", "Ré#",
-];
